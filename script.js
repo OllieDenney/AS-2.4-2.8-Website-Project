222e2ce05d34a3a6d7c63998fb274635f510aabb
OllieDenney <18odenney@wakatipu.school.nz>
2021-09-17 18:51:21 +1200

    17/9/21 - Added the complete header and nav bar to all pages. Did small polishes to all pages, including: Creating whitespace between divs, adding more improved media queries, adding links to all buttons & divs that require them, adding small transform transition to clickable buttons, added a cursor change when the user hovers over a div and many more small polishes. For the meantime this is the final developments for this website, there may be some very minor changes but nothing big is going to be added. All time will be spent on polishing the Miro Board and creating the final release.

--- script.js
+++ script.js
@@ -3,8 +3,35 @@ function showDiv() {
   var y = document.getElementById("dropdown-menu");
   if (x.style.display === "none") {
     x.style.display = "block";
+	y.innerHTML = "✖"
   }
   else {
     x.style.display = "none";
+	y.innerHTML = "☰"
   }
+}
+
+
+
+window.onscroll = function(){
+	var a = document.getElementById("nav-container");
+	var b = document.getElementById("sign-in-button-outline");
+	var c = document.getElementById("sign-in-button");
+	var d = document.getElementById("sign-in-button-outline");
+  var e = document.getElementById("dropdown");
+	var top = window.scrollY;
+	console.log(top);
+	if (top >= 100){
+		a.style.background = "white";
+		a.style.color = "#ff00e7";
+		b.style.border = "2px solid #ff00e7";
+		c.style.color = "#ff00e7";
+    e.style.background = "linear-gradient(0deg, rgba(0,0,0,0) 20%, rgba(255,255,255,1) 30%)";
+	}else{
+		a.style.background = "transparent";
+		b.style.border = "2px solid white";
+		c.style.color = "white";
+		a.style.color = "white";
+    e.style.background = "transparent";
+	}
 }
